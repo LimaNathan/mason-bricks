40298ad3f2e367f6d2be13b86cb0b3abaf81dd57
Nathan Lima <nathan.o.aguiar@gmail.com>
2023-04-01 21:55:35 -0300

    feat: add structure to log

--- bricks/feature/hooks/post_gen.dart
+++ bricks/feature/hooks/post_gen.dart
@@ -4,7 +4,6 @@ import 'package:mason/mason.dart';
 
 void run(HookContext context) async {
   final process = context.logger.progress('Building store complements');
-
   await Process.run('flutter', ['pub', 'run', 'build_runner build']);
   process.complete();
 }
--- bricks/feature/hooks/pre_gen.dart
+++ bricks/feature/hooks/pre_gen.dart
@@ -1,6 +1,23 @@
-
 import 'package:mason/mason.dart';
 
 void run(HookContext context) {
-  
+  context.logger.write('we are creating this structure');
+  final String name = context.vars['name'];
+  context.logger.write('''
+├── $name
+│ ├── domain
+│ │       ├── entities
+│ │       ├── errors
+│ │       ├── repositores
+│ │       ├── use_cases
+│ ├── external
+│ ├── infra
+│ │       ├── datasources
+│ │       ├── models
+│ │       ├── repositores
+│ ├── presenter
+│ │       ├── stores
+│ │       ├── widgets
+│ │       ├── nome_da_feature_page.dart
+    ''');
 }
